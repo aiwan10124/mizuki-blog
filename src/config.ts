6d8378fab22c005a6fcd74a9f599c49b8b770077
aiwan10124 <zy1686324434@outlook.com>
2026-05-05 07:38:20 +0800

    更改了些设置

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "༺爱玩༻的小屋",
 	subtitle: "欢迎光临？",
-	siteURL: "https://bolg.aiwan.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.aiwan.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-04-26", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
